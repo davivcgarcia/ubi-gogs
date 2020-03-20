05e2aa9bf255ed206b7f9fb16937ae652853c81a
Davi Garcia <davivcgarcia@gmail.com>
2020-03-20 10:54:21 -0300

    Use pre-defined variable names instead of default

--- helpers/run-gogs.sh
+++ helpers/run-gogs.sh
@@ -5,10 +5,10 @@ export USER_ID=$(id -u)
 export GROUP_ID=$(id -g)
 
 # Fix non-root execution
-grep -v "^${GOGS_USER}" /etc/passwd > "${HOME}/passwd"
-echo "${GOGS_USER}:x:${USER_ID}:${GROUP_ID}:gogs.io user:${HOME}:/bin/bash" >> "${HOME}/passwd"
+grep -v "^${GOGS_USER}" /etc/passwd > "${GOGS_HOMEDIR}/passwd"
+echo "${GOGS_USER}:x:${USER_ID}:${GROUP_ID}:gogs.io user:${GOGS_HOMEDIR}:/bin/bash" >> "${GOGS_HOMEDIR}/passwd"
 export LD_PRELOAD=libnss_wrapper.so
-export NSS_WRAPPER_PASSWD=${HOME}/passwd
+export NSS_WRAPPER_PASSWD=${GOGS_HOMEDIR}/passwd
 export NSS_WRAPPER_GROUP=/etc/group
 
 # Executes Gogs service
